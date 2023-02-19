632c8a0404b63ee06693241354ff49f3599fec3f
samudraon <samudraon@gmailcom>
2023-02-19 23:13:13 +0600

    template string js added

---
+++ js/template.js
@@ -0,0 +1,20 @@
+const person = 'Adam Sand';
+const person2 = "Ben White";
+const person3 = `Donald Trump`;
+
+const multiLine = 'First Line text \n' +
+    'second line of code \n' +
+    'Third line of code \n' +
+    'Fourth line of string';
+// console.log(multiLine);
+
+const newMultiLine = `First line of text
+second line of text
+third line of text
+fourth line of code`;
+console.log(newMultiLine);
+
+const a = 20;
+const b = 30;
+const newSummation = `sum of ${a} and ${b} is: ${a + b}`;
+console.log(newSummation);
