8d275fac53c09da49ed2f460ef883814bb053964
samudraon <samudraon@gmailcom>
2023-02-19 22:14:55 +0600

    function default parameter

---
+++ js/default.js
@@ -0,0 +1,13 @@
+function add(first, second = 0) {
+    const total = first + second;
+    return total;
+}
+const result = add(10);
+console.log(result);
+
+function fullName(first, last = 'Chowdhury') {
+    const name = first + ' ' + last;
+    return name;
+}
+const name = fullName('Jamshed');
+console.log(name);
